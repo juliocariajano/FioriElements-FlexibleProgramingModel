2b291a98325796190dbd2c6a6d6c4d9341d56c5d
Julio Cariajano <93696214+juliocariajano@users.noreply.github.com>
2025-08-21 14:17:06 +0200

    Creacion de controlador Sales y Fragement Sales.fragment.xml

--- app/ui-bookshop/webapp/ext/fragment/Sales.js
+++ app/ui-bookshop/webapp/ext/fragment/Sales.js
@@ -1,11 +1,20 @@
-sap.ui.define([
-    "sap/m/MessageToast"
-], function(MessageToast) {
-    'use strict';
+sap.ui.define(['sap/m/MessageToast', 'sap/ui/core/mvc/Controller', 'sap/ui/model/json/JSONModel', 'require'],
+	function (MessageToast, Controller, JSONModel, require) {
+	"use strict";
 
-    return {
-        onPress: function(oEvent) {
-            MessageToast.show("Custom handler invoked.");
-        }
-    };
+	var PageController = Controller.extend("sap.suite.ui.microchart.sample.LineMicroChart.Page", {
+
+		onInit: function () {
+			// set mock data
+			var sPath = require.toUrl("./SampleData.json");
+			var oModel = new JSONModel(sPath);
+			this.getView().setModel(oModel);
+		},
+
+		press: function() {
+			MessageToast.show("The Line Micro Chart has been pressed.");
+		}
+	});
+
+	return PageController;
 });
